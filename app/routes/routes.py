7bfd9e815600d667e547e637eef9646d9cfe79ca
Wesley Eufrasio <wesley.eufrasio@cmsw.com>
2022-06-30 16:11:08 -0300

    rotas e modificações novas

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -10,3 +10,19 @@ def root():
 @app.route('/users', methods=['POST'])
 def post_user():
     return users.post_user()
+
+@app.route('/users/<id>', methods=['PUT'])
+def post_user(id):
+    return users.update_user(id)
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return users.get_users()
+
+@app.route('/users/<id>', methods=['GET'])
+def get_user(id):
+    return users.get_user(id)
+
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    return users.delete_user(id)
--- app/views/users.py
+++ app/views/users.py
@@ -22,4 +22,58 @@ def post_user():
         return jsonify({'message': 'unable to create', 'data': {}}), 500
     
     
+def update_user(id):
+    first_name       = request.json['first_name']
+    last_name        = request.json['last_name']
+    email            = request.json['email']
+    user_name        = request.json['user_name']
+    password         = request.json['password']
     
+    user = Users.query.get(id)
+    
+    if not user:
+        return jsonify({'message': "user don't exist", 'data': {}}), 404
+    
+    password_hash    = generate_password_hash(password)
+    
+    try:
+        user.first_name = first_name
+        user.last_name  = last_name
+        user.email      = email
+        user.user_name  = user_name
+        user.password   = password_hash
+        db.session.commit()
+        result = user_schema.dump(user)
+        return jsonify({'message': 'successfully updated', 'data': result}), 201
+    except:
+        return jsonify({'message': 'unable to update', 'data': {}}), 500
+    
+def get_users():
+    users = Users.query.all()
+    if users:
+        result = users_schema.dump(users)
+        return jsonify({'message': 'successfully fetched', 'data': result})
+    
+    return jsonify({'message': 'nothing found', 'data': {}})
+
+def get_user(id):
+    user = Users.query.get(id)
+    if user:
+        result = user_schema.dump(user)
+        return jsonify({'message': 'successfully fetched', 'data': result}), 201
+    
+    return jsonify({'message': "user don't exist", 'data': {}}), 404
+
+def delete_user(id):
+    user = Users.query.get(id)
+    if not user:
+        return jsonify({'message': "user don't exist", 'data': {}}), 404
+    
+    if user:
+        try:
+            db.session.delete(user)
+            db.session.commit()
+            result = user_schema.dump(user)
+            return jsonify({'message': 'successfully deleted', 'data': result}), 200
+        except:
+            return jsonify({'message': 'unable to delete', 'data': {}}), 500
